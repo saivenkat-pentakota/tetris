df6832645aad72b354ae6a5faa7060fac639224a
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-04-24 20:01:33 +0530

    created components

---
+++ src/components/Cell.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cell({type}){
+    return(
+        <div>Cell</div>
+    )
+}
+    
+
+
+export default Cell
---
+++ src/components/StartButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StartButton({callback}) {
+
+    return(
+        <div></div>
+    )
+}
+   
+
+export default StartButton;
